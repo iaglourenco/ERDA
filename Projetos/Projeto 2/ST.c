4aeffdbe8ebe61a156cec498eeea577c2ca3b3bb
Iago <iagojlourenco@gmail.com>
2018-05-21 09:55:14 -0300

    SPS 1.1 Implementando as funcões da ST.c

--- Projetos/Projeto 2/ST.c
+++ Projetos/Projeto 2/ST.c
@@ -17,8 +17,17 @@ void criaVazia(NO **raiz){
 
 int verificaExistencia(NO *raiz,char palavra[]){
 //verifica a existencia da palavra na arvore
+//retorna 1 se for encontrada, 0 se contrário
 
+  if(!raiz) return 0; //arvore vazia
 
+  if(strcmp(raiz->palavra,palavra) == 0)  return 1;//achei a palavra
+
+  if(strcmp(raiz->palavra,palavra)<0) return verificaExistencia(raiz->esq,palavra);//vou pra esq
+
+  if(strcmp(raiz->palavra,palavra)>0) return verificaExistencia(raiz->dir,palavra);//vou pra dir
+
+return verificaExistencia(raiz->esq,palavra) && verificaExistencia(raiz->dir,palavra);//verifica a arvore inteira
 
 }
 
