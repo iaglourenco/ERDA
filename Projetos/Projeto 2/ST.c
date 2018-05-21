0497eb4975ca200dfdf166682e40162bc4ff20c4
Iago <iagojlourenco@gmail.com>
2018-05-21 10:15:56 -0300

    SPS 1.1.01 Implementando as funções da ST.c

--- Projetos/Projeto 2/ST.c
+++ Projetos/Projeto 2/ST.c
@@ -15,6 +15,31 @@ void criaVazia(NO **raiz){
   (*raiz) = NULL;
 }
 
+int inserir(NO **raiz,char palavra[]){
+//insere palavra na arvore, retorna 1 se OK, 0 se não
+  if(!(*raiz)){
+    (*raiz) = (NO *) malloc(sizeof(NO));
+
+    if(!(*raiz)) return 0; //Heap Overflow
+
+    strcpy((*raiz)->palavra,palavra);
+    (*raiz)->count=1;
+    (*raiz)->esq = (*raiz)->dir = NULL;
+    return 1; //OK
+  }
+
+  if(strcmp((*raiz)->palavra,palavra)==0)//iguais
+  {
+    (*raiz)->count++;//incrementa 1
+    return 1;//OK
+  }
+  if(strcmp((*raiz)->palavra,palavra)<0)//vou pra esq
+    return inserir(&(*raiz)->esq,palavra);
+
+  if(strcmp((*raiz)->palavra,palavra)>0)//vou pra dir
+    return inserir(&(*raiz)->dir,palavra);
+}
+
 int verificaExistencia(NO *raiz,char palavra[]){
 //verifica a existencia da palavra na arvore
 //retorna 1 se for encontrada, 0 se contrário
@@ -23,11 +48,7 @@ int verificaExistencia(NO *raiz,char palavra[]){
 
   if(strcmp(raiz->palavra,palavra) == 0)  return 1;//achei a palavra
 
-  if(strcmp(raiz->palavra,palavra)<0) return verificaExistencia(raiz->esq,palavra);//vou pra esq
-
-  if(strcmp(raiz->palavra,palavra)>0) return verificaExistencia(raiz->dir,palavra);//vou pra dir
-
-return verificaExistencia(raiz->esq,palavra) && verificaExistencia(raiz->dir,palavra);//verifica a arvore inteira
+return verificaExistencia(raiz->esq,palavra) || verificaExistencia(raiz->dir,palavra);//verifica a arvore inteira
 
 }
 
--- Projetos/Projeto 2/ST.h
+++ Projetos/Projeto 2/ST.h
@@ -3,7 +3,8 @@
 typedef struct no NO;
 
 void criaVazia(NO **raiz);
-int verificaExistencia(NO *raiz,char palavra[]);
+int verificaExistencia(NO *raiz,char palavra[]);//verifica a existencia da palavra na arvore
+int inserir(NO **raiz,char palavra[]);//faz o que o nome diz :)
 
 
 
